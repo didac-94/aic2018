d9a6861931b3a1b32c27b1fd769888d01f36142b
meave96 <40992915+meave96@users.noreply.github.com>
2018-07-22 19:33:45 +0200

    Add files via upload
    bug fix + warrior micro

---
+++ betav3.1/AttackUnit.java
@@ -0,0 +1,127 @@
+package betav3;
+
+import aic2018.Location;
+import aic2018.TreeInfo;
+import aic2018.UnitController;
+import aic2018.UnitInfo;
+
+public class AttackUnit {
+
+    UnitController uc;
+    Data data;
+    Tools tools;
+
+    public AttackUnit(){}
+
+    public AttackUnit(UnitController _uc) {
+        uc = _uc;
+        data = new Data(uc);
+        tools = new Tools(data);
+    }
+
+    void run() {
+
+        //Update our info according to the Comm Channel
+        data.Update();
+
+        //Try to attack before movement
+        attack();
+
+        //Movement
+        move();
+
+        //Try to attack after movement
+        attack();
+        attackOak();
+
+        //Report intel to the Comm Channel
+        report();
+
+    }
+
+    void report() {
+        reportMyself();
+        reportEnemies();
+        reportEnemyBases();
+    }
+
+    void reportMyself() {
+        // Report to the Comm Channel
+        uc.write(data.unitReportCh, uc.read(data.unitReportCh)+1);
+        // Reset Next Slot
+        uc.write(data.unitResetCh, 0);
+    }
+
+    void reportEnemies() {
+        UnitInfo[] enemiesAround = uc.senseUnits(data.enemyTeam);
+        if (enemiesAround.length > 0) {
+            //If there's enemies around and no enemy was reported, report an enemy
+            if (data.enemyFound == 0) {
+                Location enemyLoc = enemiesAround[0].getLocation();
+                int encryptedLoc = tools.Encrypt(enemyLoc.x, enemyLoc.y);
+                uc.write(data.enemyFoundCh, 1);
+                uc.write(data.enemyLocCh, encryptedLoc);
+            }
+        } else {
+            //If no one is around the reported enemy location reset it
+            if (data.enemyFound == 1) {
+                Location enemyLoc = tools.Decrypt(data.enemyLoc);
+                if (uc.canSenseLocation(enemyLoc)) {
+                    uc.write(data.enemyFoundCh, 0);
+                    uc.write(data.enemyLocCh, -1);
+                }
+            }
+        }
+    }
+
+    void reportEnemyBases() {
+        for (int i = data.firstEnemyBase; i < data.nEnemyBases; i++) {
+            if (uc.canSenseLocation(data.enemyBases[i])) {
+                if (uc.senseUnits(data.enemyTeam).length == 0) {
+                    uc.write(data.enemyBase0Ch + i, 0);
+                }
+            }
+        }
+    }
+
+    void attack() {
+        UnitInfo[] enemies = uc.senseUnits(data.enemyTeam);
+
+        //attack the enemy with less health
+        int minEnemyHealth = data.INF;
+        UnitInfo target = null;
+        for (UnitInfo unit : enemies) {
+            if (unit.getHealth() < minEnemyHealth && uc.canAttack(unit)){
+                minEnemyHealth = unit.getHealth();
+                target = unit;
+            }
+        }
+        if( target != null) uc.attack(target);
+    }
+
+
+
+    void attackOak() {
+        TreeInfo[] trees = uc.senseTrees();
+        for (TreeInfo tree : trees) {
+            if (tree.oak) {
+                if (uc.canAttack(tree)) {
+                    uc.attack(tree);
+                }
+            }
+        }
+    }
+
+    void move() {
+        Location target;
+        if (data.enemyFound == 0) {
+            Location enemyBase = tools.Barycenter(data.enemyTeam.getInitialLocations());
+            Location allyBase = new Location(data.workerX/data.nWorker,data.workerY/data.nWorker);
+            target = tools.BarCoord(new Location[] {enemyBase, allyBase}, new int[] {1,3});
+        } else {
+            target = tools.Decrypt(data.enemyLoc);
+        }
+        tools.MoveTo(target);
+    }
+
+}
---
+++ betav3.1/Warrior.java
@@ -0,0 +1,80 @@
+package betav3;
+
+import aic2018.Location;
+import aic2018.UnitController;
+import aic2018.UnitInfo;
+
+public class Warrior extends AttackUnit {
+
+    public Warrior(UnitController _uc) {
+        super(_uc);
+    }
+
+    @Override
+    void run() {
+
+        //Update our info according to the Comm Channel
+        data.Update();
+
+
+        //Try to attack before movement
+        attack();
+        counter();
+
+        //Movement
+        move();
+
+        //Try to attack after movement
+        attack();
+        counter();
+        attackOak();
+
+        //Report intel to the Comm Channel
+        report();
+    }
+
+    @Override
+    void report() {
+        reportMyself();
+        reportEnemies();
+        reportEnemyBases();
+    }
+
+    @Override
+    void reportMyself() {
+        // Report to the Comm Channel
+        uc.write(data.unitReportCh, uc.read(data.unitReportCh)+1);
+        uc.write(data.warriorReportCh, uc.read(data.warriorReportCh)+1);
+        // Reset Next Slot
+        uc.write(data.unitResetCh, 0);
+        uc.write(data.warriorResetCh, 0);
+    }
+
+    @Override
+    void move() {
+        Location target;
+        if (uc.getRound() < 700 && data.enemyFound == 0) {
+            Location enemyBase = tools.Barycenter(data.enemyBases);
+            Location allyBase = new Location(data.workerX/data.nWorker,data.workerY/data.nWorker);
+            target = tools.BarCoord(new Location[] {enemyBase, allyBase}, new int[] {1,3});
+        } else {
+            target = tools.Decrypt(data.enemyLoc);
+        }
+        tools.MoveTo(target);
+    }
+
+    void counter() {
+        UnitInfo[] units = uc.senseUnits(data.enemyTeam);
+        int maxEnemyHealth = 0;
+        UnitInfo target = null;
+
+        //aplicar counter sobre l'enemic amb mes vida
+        for(UnitInfo unit : units){
+            if(unit.getHealth() > maxEnemyHealth && uc.canUseActiveAbility(unit.getLocation())){
+                maxEnemyHealth = unit.getHealth();
+                target = unit;
+            }
+        }
+        if (target != null) uc.useActiveAbility(target.getLocation());
+    }
+}
